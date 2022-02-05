8bcdf420fcb540b93adfb0ca1b92d180f1954343
Talha156428 <73047840+Talha156428@users.noreply.github.com>
2022-02-05 17:36:22 -0600

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,15 @@
+import curses
+
+curses.initscr()
+win = curses.newwin(20, 60, 0, 0)  # y,x
+win.keypad(1)
+curses.noecho()
+curses.curs_set(0)
+win.border(0)
+win.nodelay(1)  # -1
+
+score = 0
+while True:
+    event = win.getch()
+curses.endwin()
+print(f"final score = {score}")
